6a01190cc3ebe45495491d2cb91cdb36f02f797d
MoneyManClyd <96083863+MoneyManClyd@users.noreply.github.com>
2025-07-10 12:23:40 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -260,7 +260,7 @@ export default function Home() {
                 <div className="aspect-video rounded-lg overflow-hidden gradient-border">
                   <iframe
                     className="w-full h-full"
-                    src="https://www.youtube.com/embed/_ycJBvzbZdQ"
+                    src="https://www.youtube.com/embed/pVP1oLN2HoA"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
